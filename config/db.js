1877664fd8a85f4e5c1c7d7f5b7064550f492abc
Randall Reichenberger <randall.reichenberger1@gmail.com>
2024-07-25 13:19:49 -0500

    Config Adjustments

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
 require('dotenv').config(); 
 const { Client } = require('pg')
-const { getDatabaseUri } = require('./config/config');
+const { getDatabaseUri } = require('./config');
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(getDatabaseUri(), {
   dialect: 'postgres',
   logging: true
 });
+
+console.log("Database URI:", getDatabaseUri());
+
 
 module.exports = {
   sequelize
--- models/index.js
+++ models/index.js
@@ -1,19 +1,12 @@
+require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('sequelize');
+const { sequelize } = require('../config/db'); // Import the pre-configured sequelize instance
+const { Sequelize } = require('sequelize'); // Import Sequelize for model definitions
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
 
-// Import all models and associate them
 fs.readdirSync(__dirname)
   .filter(file => {
     return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
@@ -40,7 +33,9 @@ Object.keys(db).forEach(modelName => {
 //   }
 // })();
 
+// Export sequelize and models
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+
